a1e8ca9c30bc38af0d66ef81e9c328a15d742649
PERFORMAIT\dasantos <daiane.santos@performait.com>
2018-03-25 10:33:05 -0300

    v5

--- src/app/components/adm/alterar-senha/alterar-senha.component.ts
+++ src/app/components/adm/alterar-senha/alterar-senha.component.ts
@@ -5,7 +5,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { toast } from 'angular2-materialize';
 
 /** Models */
-import { Motorista } from '../../../models/motorista.model'
+import { Usuario } from '../../../models/usuario.model';
 
 /** Services */
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
@@ -25,9 +25,8 @@ export class AlterarSenhaComponent implements OnInit {
 
   @ViewChild('formEditMotorista') formEditMotorista: NgForm;
 
-  public motoristas: Array<Motorista> = [];
-  public motorista: Motorista = new Motorista();
-  public loading: any = false;
+  public usuarioModel: Usuario = new Usuario();
+
   public loadingText: string = '';
 
   constructor(
@@ -35,35 +34,27 @@ export class AlterarSenhaComponent implements OnInit {
     private route: ActivatedRoute,
     public domSanitizer: DomSanitizer,
     private spinnerService: Ng4LoadingSpinnerService,
-    private driverVanService: DriverVanService 
+    private driverVanService: DriverVanService
     //private flashMessagesService: FlashMessagesService
   ) {
 
   }
 
   public ngOnInit() {
-    this.listarMotoristas();
+
   }
 
-  public listarMotoristas() {
-    this.loadingText = 'Aguarde...';
-    this.spinnerService.show();
-    this.driverVanService.getAllDriverVan().subscribe(
-      resp => {
-        this.spinnerService.hide();
-        if(resp['isSucceed']){
-          this.motoristas = resp['data'];
-        }
-      },
-      err => {
-        this.spinnerService.hide();
-        toast('<i class="material-icons">notifications</i>&nbsp;<span>Requisição Inválida!</span>'
-          , 5000, 'orange darken-3');
-      }
-    )
+  public submitFormAlterarSenha(form) {
+    if (form.form.status === "VALID") {
+      console.log(this.usuarioModel);
+      this.alterarSenha(this.usuarioModel);
+    }
+    else {
+
+    }
   }
 
-  public cadastrarMotorista(form) {
+  public alterarSenha(form) {
     this.loadingText = 'Aguarde...';
     this.spinnerService.show();
     this.driverVanService.setDriverVan(form.value).subscribe(
@@ -77,21 +68,5 @@ export class AlterarSenhaComponent implements OnInit {
       }
     )
   }
-
-  public editarMotorista(form) {
-    this.loadingText = 'Aguarde...';
-    this.spinnerService.show();
-    this.driverVanService.setDriverVan(form.value).subscribe(
-      resp => {
-        this.spinnerService.hide();
-      },
-      err => {
-        this.spinnerService.hide();
-        toast('<i class="material-icons">notifications</i>&nbsp;<span>Requisição Inválida!</span>'
-          , 5000, 'orange darken-3');
-      }
-    )
-  }
-  
 
 }
